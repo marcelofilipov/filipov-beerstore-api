d2dcca4395b42d6c1fed52fa8bee38e95c08ab3c
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-04-30 00:27:25 -0300

    The first commit

---
+++ src/main/java/com/filipov/beerstore/api/FilipovBeerstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.filipov.beerstore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilipovBeerstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilipovBeerstoreApiApplication.class, args);
+	}
+
+}
