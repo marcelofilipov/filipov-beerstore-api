83c8ce5adf86b11be1513503bb564273a7886d11
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-05-08 22:18:38 -0300

    Implemented InvalidFormat

--- src/main/java/com/filipov/beerstore/api/error/ApiExceptionHandler.java
+++ src/main/java/com/filipov/beerstore/api/error/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.filipov.beerstore.api.error;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.filipov.beerstore.api.error.ErrorResponse.ApiError;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -39,13 +40,24 @@ public class ApiExceptionHandler {
                 .collect(toList());
 
         ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, apiErrors);
+
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
-    private ApiError toApiError(String code, Locale locale) {
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidFormatException(InvalidFormatException exception
+            , Locale locale) {
+        final String errorCode = "generic-1";
+
+        final ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, toApiError(errorCode, locale, exception.getValue()));
+
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    private ApiError toApiError(String code, Locale locale, Object... args) {
         String message;
         try {
-            message = apiErrorMessageSource.getMessage(code, null, locale);
+            message = apiErrorMessageSource.getMessage(code, args, locale);
         } catch (NoSuchMessageException e) {
             LOG.error("Could not find any message for {} code under {} locale", code, locale);
             message = NO_MESSAGE_AVAILABLE;
