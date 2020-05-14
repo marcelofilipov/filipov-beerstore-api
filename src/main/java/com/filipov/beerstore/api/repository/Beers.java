25ebe9958ffb9e63028e3adc5069157a3ebffff7
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-05-13 21:41:32 -0300

    Implemented tests with Hamcrest and Mockito

--- src/main/java/com/filipov/beerstore/api/repository/Beers.java
+++ src/main/java/com/filipov/beerstore/api/repository/Beers.java
@@ -1,7 +1,13 @@
 package com.filipov.beerstore.api.repository;
 
 import com.filipov.beerstore.api.model.Beer;
+import com.filipov.beerstore.api.model.BeerType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface Beers extends JpaRepository<Beer, Long> {
+
+    Optional<Beer> findByNameAndType(String name, BeerType type);
+
 }
