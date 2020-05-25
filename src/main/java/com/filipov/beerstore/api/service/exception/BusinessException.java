0a217d9c89bb9ebba5bc595fd533b1f995d201a6
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-05-25 19:20:06 -0300

    Handling the business exception

---
+++ src/main/java/com/filipov/beerstore/api/service/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package com.filipov.beerstore.api.service.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final String code;
+    private final HttpStatus status;
+
+}
