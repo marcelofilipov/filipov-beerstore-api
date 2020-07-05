8efb4b0d77bed89cf4af80b11eaebf45be3adfe2
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-07-05 18:00:01 -0300

    Configuration Ansible and Swarm

--- terraform/security.tf
+++ terraform/security.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "allow_ssh" {
       from_port = 22
       to_port = 22
       protocol = "tcp"
-      cidr_blocks = ["191.13.109.163/32"]
+      cidr_blocks = ["${var.my_public_ip}"]
   }
 }
 
@@ -19,5 +19,50 @@ resource "aws_security_group" "database" {
       to_port = 5432
       protocol = "tcp"
       self = true
+  }
+}
+
+resource "aws_security_group" "allow_outbound" {
+  vpc_id = aws_vpc.main.id
+  name = "filservtech_allow_outbound"
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "cluster_communication" {
+  vpc_id = aws_vpc.main.id
+  name = "filservtech_cluster_communication"
+
+  ingress {
+    from_port = 2377
+    protocol = "tcp"
+    to_port = 2377
+    self = true
+  }
+
+  ingress {
+    from_port = 7946
+    protocol = "tcp"
+    to_port = 7946
+    self = true
+  }
+
+  ingress {
+    from_port = 7946
+    protocol = "udp"
+    to_port = 7946
+    self = true
+  }
+
+  ingress {
+    from_port = 4789
+    protocol = "udp"
+    to_port = 4789
+    self = true
   }
 }
